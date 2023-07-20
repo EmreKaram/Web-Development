b78dfb2ca44e4cb76aad78357c29796a9839dce3
Emre Karam <emre-karamahmut-@hotmail.com>
2023-07-20 18:23:14 +0300

    Drum kit files added

--- 17. Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/index.js
+++ 17. Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/index.js
@@ -1,15 +1,17 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document
-  .querySelector(".img1")
-  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document
-  .querySelector(".img2")
-  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
-} else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-} else {
-  document.querySelector("h1").innerHTML = "Draw!";
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
