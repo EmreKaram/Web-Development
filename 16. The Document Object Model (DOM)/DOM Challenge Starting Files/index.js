bb5608f00a2afba6ec5cc26be02c839242e54dd3
Emre Karam <emre-karamahmut-@hotmail.com>
2023-07-20 04:25:25 +0300

    continue with section 16

---
+++ 16. The Document Object Model (DOM)/DOM Challenge Starting Files/index.js
@@ -0,0 +1,11 @@
+document.query
+
+document.getElementById("#h1").style.textAlign = "center"; 
+
+document.getElementsByTagName("li")[0].style.color = "blue"; 
+
+document.querySelector("#undordered").style.color = "blue"; 
+
+document.querySelectorAll(".list")[2].style.fontSize = "2rem"; // For specific items in the list you should use [0], [1], etc.
+
+document.getElementsByClassName("list")[1].style.backgroundColor = "yellow";
--- 16. The Document Object Model (DOM)/script.js
+++ 16. The Document Object Model (DOM)/script.js
@@ -1,3 +1,8 @@
 function sayHello() {
   alert("Hello World!");
 }
+
+function changeColor() {
+    var text = document.getElementById("text");
+    text.style.color = "blue";
+}
