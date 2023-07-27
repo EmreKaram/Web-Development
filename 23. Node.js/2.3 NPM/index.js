880fe6acf675818c554bdd2c1380046b55aeb7b1
Emre Karam <emre-karamahmut-@hotmail.com>
2023-07-28 00:42:31 +0300

    not finished Node.js

---
+++ 23. Node.js/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+// var generateName = require("sillyname");
+import generateName from "sillyname";
+
+var sillyname = generateName();
+console.log(`My name is ${sillyname}.`);
+
+import superheroes from "superheroes";
+
+var mySuperheroName = superheroes.random();
+console.log(`My superhero name is ${mySuperheroName}! and I'am here to save you ...`);
