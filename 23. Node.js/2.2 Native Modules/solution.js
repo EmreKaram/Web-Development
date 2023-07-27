4cb7059b7cfc5ba22a68dccfc2ecdd9aced14fa4
Emre Karam <emre-karamahmut-@hotmail.com>
2023-07-27 19:50:40 +0300

    Welcome to node.js

---
+++ 23. Node.js/2.2 Native Modules/solution.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+fs.writeFile("message.txt", "Hello Node", (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});
+
+fs.readFile("message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
