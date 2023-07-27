09e15ea51cf2496988e54bd9bd95d13ef4889bd6
Emre Karam <emre-karamahmut-@hotmail.com>
2023-07-27 18:31:15 +0300

    End of the section 22

--- 20. Boss Level Chalenge 2 - The Simon Game/game.js
+++ 20. Boss Level Chalenge 2 - The Simon Game/game.js
@@ -34,7 +34,7 @@ function checkAnswer(currentLevel) {
   } else {
     playSound("wrong");
     $("body").addClass("game-over");
-    $("#level-title").text("Game Over, Press Any Key to Restart")
+    $("#level-title").text("Game Over, Press Any Key to Restart");
     setTimeout(function () {
       $("body").removeClass("game-over");
     }, 200);
