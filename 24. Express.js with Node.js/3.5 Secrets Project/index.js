be9d0a82a6f06af3083d05b99d9cf368f90e4b50
Emre Karam <emre-karamahmut-@hotmail.com>
2023-07-31 23:26:13 +0300

    Custom middleware that checks password

---
+++ 24. Express.js with Node.js/3.5 Secrets Project/index.js
@@ -0,0 +1,40 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var value = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  if (req.body["password"] === "1546") {
+    value = true;
+  }
+  next();
+}
+
+app.use(passwordCheck);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (value) {
+    res.sendFile(__dirname + "/public/secret.html");
+} else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
