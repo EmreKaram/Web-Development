7433d99632476d9a157d310e6b0b73445cf9ec79
Emre Karam <emre-karamahmut-@hotmail.com>
2023-07-31 22:40:21 +0300

    Custom Middleware (bandName)

--- 24. Express.js with Node.js/3.4 Middleware/index2.js
+++ 24. Express.js with Node.js/3.4 Middleware/index2.js
@@ -1,7 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+app.use(morgan("tiny"));
+
+app.get("/", function (req, res) {
+  res.send("hello, world!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello");
--- 24. Express.js with Node.js/3.4 Middleware/index3.js
+++ 24. Express.js with Node.js/3.4 Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
