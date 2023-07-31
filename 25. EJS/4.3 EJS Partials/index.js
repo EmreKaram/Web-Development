f1760b6d64cbbb59fdc6883121ed1f746125d8e2
Emre Karam <emre-karamahmut-@hotmail.com>
2023-07-31 23:43:13 +0300

    EJS lessons module are added

---
+++ 25. EJS/4.3 EJS Partials/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+/* Write your code here:
+Step 1: Render the home page "/" index.ejs
+Step 2: Make sure that static files are linked to and the CSS shows up.
+Step 3: Add the routes to handle the render of the about and contact pages.
+  Hint: Check the nav bar in the header.ejs to see the button hrefs
+Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
