64c0f84a0ea4c441f535c17a8ab899532d891684
Emre Karam <emre-karamahmut-@hotmail.com>
2023-08-25 21:26:38 +0300

    Weather APP Capstone project done and added

---
+++ 29. Capstone Project API/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://api.openweathermap.org";
+const apiKey = "e4982a616287eae4d4a454f56c187c9d";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "Waiting for data..." });
+});
+
+app.post("/get-weather", async (req, res) => {
+  const cityName = req.body.city;
+  try {
+    const result = await axios.get(
+      API_URL + `/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
+    );
+    const temperature = result.data.main.temp;
+    res.render("index.ejs", { content: JSON.stringify(result.data), temperature });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500).send("An error occurred");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
