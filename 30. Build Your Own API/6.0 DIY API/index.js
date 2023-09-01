a589ac7482024ffe2f48ce1c51fa48677eddff19
Emre Karam <emre-karamahmut-@hotmail.com>
2023-09-01 22:57:15 +0300

    Delete and All Delete are done. 6.1 Blog doc added

--- 30. Build Your Own API/6.0 DIY API/index.js
+++ 30. Build Your Own API/6.0 DIY API/index.js
@@ -62,9 +62,30 @@ app.patch("/jokes/:id", (req, res) => {
   res.json(replacementJoke);
 });
 //7. DELETE Specific joke
-
+app.delete("/jokes/:id", (req, res) => {
+  const id = parent(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id:${id} not found. No jokes were deleted.` });
+  }
+});
 //8. DELETE All jokes
-
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `You are not authorised to perform this action.` });
+  }
+});
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
 });
