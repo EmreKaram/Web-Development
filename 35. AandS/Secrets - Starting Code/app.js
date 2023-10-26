a3faa56ff103cbd9ad765ceade84de08edfdc078
Emre Karam <emre-karamahmut-@hotmail.com>
2023-10-26 17:27:20 +0300

    User Login and Register pages backend are done

--- 35. AandS/Secrets - Starting Code/app.js
+++ 35. AandS/Secrets - Starting Code/app.js
@@ -2,8 +2,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -19,6 +29,41 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", async function (req, res) {
+  try {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
+
+    await newUser.save();
+    res.render("secrets");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post("/login", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    const foundUser = await User.findOne({ email: username });
+
+    if (foundUser) {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      } else {
+        res.send("Wrong password");
+      }
+    } else {
+      res.send("User not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.send("An error occurred");
+  }
 });
 
 app.listen(3000, function () {
