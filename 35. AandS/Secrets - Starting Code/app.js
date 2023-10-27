e22849af13f767112e6d172990c05025ed307042
Emre Karam <emre-karamahmut-@hotmail.com>
2023-10-27 13:29:14 +0300

    UPDATE

--- 35. AandS/Secrets - Starting Code/app.js
+++ 35. AandS/Secrets - Starting Code/app.js
@@ -1,10 +1,14 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
@@ -12,6 +16,12 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
+
+userSchema.plugin(
+  encrypt,
+  { secret: process.env.SECRET },
+  { encryptedFields: ["password"] }
+);
 
 const User = new mongoose.model("User", userSchema);
 
