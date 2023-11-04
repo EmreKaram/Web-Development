63a75755145adf0a0ffeae1b58a113f88a233087
Emre Karam <emre.karamahmut@hotmail.com>
2023-11-04 22:25:16 +0300

    Quiz solved

--- 34.5 PostgreSQL/8.2 Postgres Read/index.js
+++ 34.5 PostgreSQL/8.2 Postgres Read/index.js
@@ -1,8 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "1546",
+  port: 5432,
+});
 
 const app = express();
 const port = 3000;
+
+db.connect();
+
+let quiz = [];
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
