8ab4015acd2bf39e3f8c83be96ec33e3af7e8b02
Emre Karam <emre.karamahmut@hotmail.com>
2023-11-04 23:23:56 +0300

    hw

--- 34.5 PostgreSQL/8.3 Travel Tracker/index.js
+++ 34.5 PostgreSQL/8.3 Travel Tracker/index.js
@@ -1,14 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "1546",
+  port: 5432,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//GET Home Page
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(result.rows);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
 });
 
 app.listen(port, () => {
