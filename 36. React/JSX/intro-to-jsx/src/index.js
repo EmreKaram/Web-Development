e4653bf2b7350c12e1e373fd19edf80edd1f904a
Emre <89421012+EmreKaram@users.noreply.github.com>
2024-01-18 19:18:01 +0300

    Refactor code to use ES6 import syntax and add styling to elements

--- 36. React/JSX/intro-to-jsx/src/index.js
+++ 36. React/JSX/intro-to-jsx/src/index.js
@@ -1,4 +1,76 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+const fName = "Emre";
+
+const lName = "Karam";
+
+const luckNum = 3;
+
+const currentDate = new Date();
+
+const currentYear = currentDate.getFullYear();
+
+const randomImg = "https://picsum.photos/200";
+
+const customStyleTest = {
+  color: "red",
+  fontSize: "15px",
+  border: "1px solid black",
+};
+
+var styleTime = {
+  color: "black",
+  fontSize: "20px",
+};
+var timeMessage = "";
+
+if (currentDate.getHours() < 12) {
+  // 0-11 is AM
+  styleTime.color = "red";
+  styleTime.fontSize = "20px";
+  timeMessage = "Good Morning";
+} else if (currentDate.getHours() < 18) {
+  // 12-17 is PM
+  styleTime.color = "green";
+  styleTime.fontSize = "20px";
+  timeMessage = "Good Afternoon";
+} else {
+  // 18-23 is PM
+  styleTime.color = "blue";
+  styleTime.fontSize = "20px";
+  timeMessage = "Good Night";
+}
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      {`${fName} ${lName}`}'s React Note
+    </h1>
+    <ul>
+      <li>This is how we make list</li>
+      <li>Another bullet point example</li>
+    </ul>
+    <p>This is how we put expression example: {luckNum}</p>
+    <p>Random number: {Math.floor(Math.random() * 10)}</p>
+    <p>Copyright year {currentYear}</p>
+    <div>
+      <img
+        className="test-img"
+        src="https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68"
+        alt="random"
+      />
+      <img
+        className="test-img"
+        src="https://fastly.picsum.photos/id/13/2500/1667.jpg?hmac=SoX9UoHhN8HyklRA4A3vcCWJMVtiBXUg0W4ljWTor7s"
+        alt="random"
+      />
+      <img src={randomImg + "?grayscale"} alt="Gray Scale Random" />
+    </div>
+    <h4 style={{ color: "blue" }}>Inline style change</h4>
+    <h5 style={customStyleTest}>Custom style object</h5>
+    <h1 style={styleTime}>{timeMessage}</h1>
+  </div>,
+  document.getElementById("root")
+);
