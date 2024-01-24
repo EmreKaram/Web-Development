c3d8ddadb84bd885077c7c96c604a54c37f5a66c
Emre <89421012+EmreKaram@users.noreply.github.com>
2024-01-24 18:51:16 +0300

    Refactor Avatar component and update Card component to use Avatar component

--- 36. React/props-practise/src/components/App.jsx
+++ 36. React/props-practise/src/components/App.jsx
@@ -8,19 +8,19 @@ function App() {
       <h1 className="heading">My Contacts</h1>
       <Card
         name={contacts[0].name}
-        imgURL={contacts[0].imgURL}
+        img={contacts[0].imgURL}
         tel={contacts[0].tel}
         email={contacts[0].email}
       />
       <Card
         name={contacts[1].name}
-        imgURL={contacts[1].imgURL}
+        img={contacts[1].imgURL}
         tel={contacts[1].tel}
         email={contacts[1].email}
       />
       <Card
         name={contacts[2].name}
-        imgURL={contacts[2].imgURL}
+        img={contacts[2].imgURL}
         tel={contacts[2].tel}
         email={contacts[2].email}
       />
