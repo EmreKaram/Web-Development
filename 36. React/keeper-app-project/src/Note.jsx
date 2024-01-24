3cb7919f16623b9ab1ae29e4dbfa2b7f884c253d
Emre <89421012+EmreKaram@users.noreply.github.com>
2024-01-24 16:35:07 +0300

    Add React components and styles

---
+++ 36. React/keeper-app-project/src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Teras</h1>
+      <p>Terastan amerika hatırları alınacak!</p>
+    </div>
+  );
+}
+
+export default Note;
