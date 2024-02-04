ab9a83bc2ab97a9a3147d718c449e4de7e4fd320
Emre <89421012+EmreKaram@users.noreply.github.com>
2024-02-04 20:05:32 +0300

    Remove unnecessary files and code

--- 36. React/map-filter-reduce/src/index.js
+++ 36. React/map-filter-reduce/src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var numbers = [3, 56, 2, 48, 5];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//Map -Create a new array by doing something with each item in an array.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function double(x) {
+//   return x * 2;
+// }
+
+// const newNumbers = numbers.map(double);
+
+// ---------------OR----------------
+
+// var newNumbers = [];
+// numbers.forEach(function (x) {
+//   newNumbers.push(x * 2);
+// });
+
+// const newNumbers = numbers.map(function (x) {
+//   return x * 2;
+// });
+
+// console.log(newNumbers);
+
+//Filter - Create a new array by keeping the items that return true.
+
+// const newNumbers = numbers.filter(function (num) {
+//   return num > 10;
+// });
+
+// ---------------OR----------------
+
+// var newNumbers = [];
+// numbers.forEach(function (num) {
+//   if (num > 10) {
+//     newNumbers.push(num);
+//   }
+// });
+
+// console.log(newNumbers);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//   console.log("accumulator = " + accumulator);
+//   console.log("currentNumber = " + currentNumber);
+//   return accumulator + currentNumber;
+// });
+
+// ---------------OR----------------
+
+// var newNumber = 0;
+// numbers.forEach(function (currentNumber) {
+//   newNumber += currentNumber;
+// });
+
+// console.log(newNumber);
+
+//Find - find the first item that matches from an array.
+
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// });
+
+// console.log(newNumber);
+
+//FindIndex - find the index of the first item that matches.
+
+// const newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// });
+
+// console.log(newNumber);
