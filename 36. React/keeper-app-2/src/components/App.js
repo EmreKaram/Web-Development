cd287c124a3ff261a2109fd9283efd64703da230
Emre <89421012+EmreKaram@users.noreply.github.com>
2024-02-04 20:54:31 +0300

    Add React components and styles

---
+++ 36. React/keeper-app-2/src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
