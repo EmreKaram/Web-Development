79c8b4d8bee993078f353fe6cc207cb24f0211da
Emre <89421012+EmreKaram@users.noreply.github.com>
2024-02-04 20:34:15 +0300

    Add styles and components for emojipedia app

--- 36. React/map-filter-reduce/src/index.js
+++ 36. React/map-filter-reduce/src/index.js
@@ -1,4 +1,4 @@
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
@@ -70,3 +70,48 @@ var numbers = [3, 56, 2, 48, 5];
 // });
 
 // console.log(newNumber);
+
+// import emojipedia from "./emojipedia";
+
+// const newEmojipedia = emojipedia.map(function (emojiEntry) {
+//   // map() will return a new array
+//   return emojiEntry.meaning.substring(0, 100);
+// }); // .substring(0, 100) will only return the first 100 characters of the meaning
+
+// console.log(newEmojipedia);
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+
+// var numbers = [3, 56, 2, 48, 5];
+
+// const newNumbers = numbers.map((x) => x * x); // If there is only one parameter, you can remove the parentheses like (x) to x
+
+// console.log(newNumbers);
+
+// //Map -Create a new array by doing something with each item in an array.
+// const newNumbersMap = numbers.map((x) => x * 2);
+// console.log(newNumbersMap);
+
+// ////Filter - Create a new array by keeping the items that return true.
+// const newNumbersFilter = numbers.filter((num) => num < 10);
+// console.log(newNumbersFilter);
+
+// // Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumberReduce = numbers.reduce(
+//   (accumulator, currentNumber) => accumulator + currentNumber
+// );
+// console.log(newNumberReduce);
+
+// //Find - find the first item that matches from an array.
+// const newNumberFind = numbers.find((num) => num > 10);
+// console.log(newNumberFind);
+
+// //FindIndex - find the index of the first item that matches.
+// const newNumberFindIndex = numbers.findIndex((num) => num > 10);
+// console.log(newNumberFindIndex);
